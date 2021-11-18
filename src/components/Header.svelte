6d376ef83e7a117af4b1b2e3bbf2876f95a5ddc3
Brian Ginsburg <gins@brianginsburg.com>
2021-11-18 15:38:11 -0800

    Add link to GitHub repository

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
-  import { SkipToContent, Header } from 'carbon-components-svelte';
+  import {
+    SkipToContent,
+    Header,
+    HeaderActionLink,
+    HeaderUtilities
+  } from 'carbon-components-svelte';
+  import LogoGithub20 from 'carbon-icons-svelte/lib/LogoGithub20';
 </script>
 
 <Header company="Fission" platformName="UCAN Check" href="/">
   <div slot="skip-to-content">
     <SkipToContent />
   </div>
+  <HeaderUtilities>
+    <HeaderActionLink
+      aria-label="GitHub Repository"
+      icon={LogoGithub20}
+      href="https://github.com/ucan-wg/ucan-check"
+      target="_blank"
+    />
+  </HeaderUtilities>
 </Header>
 
 <style>
