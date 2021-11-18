5895b7082d5b304e0a13720f0b3f513e2e966253
Brian Ginsburg <gins@brianginsburg.com>
2021-11-18 15:06:11 -0800

    Add invalid UCAN example
    And some formatting.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import {
     Button,
+    ButtonSet,
     CodeSnippet,
     Column,
     InlineNotification,
@@ -54,6 +55,12 @@
   const showExample = () => {
     encodedUcan =
       'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCIsInVjdiI6IjAuNy4wIn0=.eyJhdWQiOiJkaWQ6a2V5Ono2TWt0WVVycTNLZkplUzZVd1VIWUhkTFM2NGQxek5GVFI2dDUxWnBwMWV3WnVOcyIsImF0dCI6W3sid25mcyI6ImRlbW91c2VyLmZpc3Npb24ubmFtZS9wdWJsaWMvcGhvdG9zLyIsImNhcCI6Ik9WRVJXUklURSJ9XSwiZXhwIjoyNjM3MjUyNzc0LCJpc3MiOiJkaWQ6a2V5Ono2TWtzdWhjcXVXeHR5cndMbTZ1WXJNOU1hSE5UQ01qTmRWa3R0YjFvTXF3cnA0SyIsIm5iZiI6MTYzNzI1MjcxNCwicHJmIjoiZXlKaGJHY2lPaUpGWkVSVFFTSXNJblI1Y0NJNklrcFhWQ0lzSW5WamRpSTZJakF1Tnk0d0luMD0uZXlKaGRXUWlPaUprYVdRNmEyVjVPbm8yVFd0emRXaGpjWFZYZUhSNWNuZE1iVFoxV1hKTk9VMWhTRTVVUTAxcVRtUldhM1IwWWpGdlRYRjNjbkEwU3lJc0ltRjBkQ0k2VzNzaWQyNW1jeUk2SW1SbGJXOTFjMlZ5TG1acGMzTnBiMjR1Ym1GdFpTOXdkV0pzYVdNdmNHaHZkRzl6THlJc0ltTmhjQ0k2SWs5V1JWSlhVa2xVUlNKOVhTd2laWGh3SWpveU5qTTNNalV5TnpjMExDSnBjM01pT2lKa2FXUTZhMlY1T25vMlRXdHJXbUoxVUhSQ2IyMVNaMFJOUVUxWVRESnVTREUzUm1wb1dtVlFSMlUxVTBSVVdUSldhbnBCYVVWUVZ5SXNJbTVpWmlJNk1UWXpOekkxTWpjeE5Dd2ljSEptSWpwdWRXeHNmUT09Ll9kZXpRNVdVRjNkTXpKODNHQnktY2RqZExXUnJrUTNLMzBxODhjMFRQVVpLajJCX0lGYXdYbUFaR2lGRjZBX0ZNcE5fbGg0R0xiVmJybDhPZzF4OENnPT0ifQ==.TN0sO-5no3F0pNkPksWh5RdWQrfz3nZaQzwBMjHkKnSpzfC4b5vrYU8nozU9DmzQMNUZ9fUO888DznuIQEagDQ==';
+    decode({ target: { value: encodedUcan } });
+  };
+
+  const showInvalidExample = () => {
+    encodedUcan =
+      'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCIsInVjdiI6IjAuNy4wIn0=.eyJhdWQiOiJkaWQ6a2V5Ono2TWtzaExTcUNvbjFNcTVZNll3TTRoSDJpUmNwbnpUWHA5V0Z4cGhvM2Rlc25IYiIsImF0dCI6W3sid25mcyI6ImRlbW91c2VyLmZpc3Npb24ubmFtZS9wdWJsaWMvcGhvdG9zLyIsImNhcCI6Ik9WRVJXUklURSJ9XSwiZXhwIjoxNjM3MTc0NDc4LCJpc3MiOiJkaWQ6a2V5Ono2TWtzaExTcUNvbjFNcTVZNll3TTRoSDJpUmNwbnpUWHA5V0Z4cGhvM2Rlc25IYiIsIm5iZiI6MTYzNzE3NDQxOCwicHJmIjoiZXlKaGJHY2lPaUpGWkVSVFFTSXNJblI1Y0NJNklrcFhWQ0lzSW5WamRpSTZJakF1Tnk0d0luMD0uZXlKaGRXUWlPaUprYVdRNmEyVjVPbm8yVFd0d2FVMDRkMU42UkhwWlkxZHFla3BCVVhKV2J6Vm9aM3BOYm5kQ1RGbDZWMlkwZWtacFRISldhVmx5VnlJc0ltRjBkQ0k2VzNzaWQyNW1jeUk2SW1SbGJXOTFjMlZ5TG1acGMzTnBiMjR1Ym1GdFpTOXdkV0pzYVdNdmNHaHZkRzl6THlJc0ltTmhjQ0k2SWs5V1JWSlhVa2xVUlNKOVhTd2laWGh3SWpveE5qTTNNVGMwTkRjNExDSnBjM01pT2lKa2FXUTZhMlY1T25vMlRXdGxjSGRXYlZGVVdtTmpZV1pvU0hGYVRtSlFaV1ZMWVZWT09GcDRjazVWT0RSTWVHdHBSbTl4UkRsak1TSXNJbTVpWmlJNk1UWXpOekUzTkRReE9Dd2ljSEptSWpwdWRXeHNmUT09LlBKZjY4aFlsMF9KYW9NQ1RrTklhdlR3cnhCOThoUkZvTmg4aldIOHJXN3JRRm1oZ2UzWTRrYlhucDBnTFBHTkJGWnpRZmdiZFVIYVM2eFpyVGZCZEFnPT0ifQ==.PJf68hYl0_JaoMCTkNIavTwrxB98hRFoNh8jWH8rW7rQFmhge3Y4kbXnp0gLPGNBFZzQfgbdUHaS6xZrTfBdAg==';
     decode({ target: { value: encodedUcan } });
   };
 
@@ -116,7 +123,12 @@
     {#if encodedUcan === ''}
       <Row>
         <Column>
-          <Button on:click={showExample}>Show Example UCAN</Button>
+          <ButtonSet>
+            <Button on:click={showExample}>Show Example</Button>
+            <Button kind="secondary" on:click={showInvalidExample}>
+              Show Invalid Example
+            </Button>
+          </ButtonSet>
         </Column>
       </Row>
     {/if}
@@ -208,87 +220,83 @@
       </Column>
     </Row>
   {:else}
-  <Row>
-    <Column>
-      <InlineNotification
-        kind="success"
-        title="Valid UCAN."
-        subtitle="The UCAN is valid and has not expired."
-        lowContrast
-      />
-    </Column>
-  </Row>
+    <Row>
+      <Column>
+        <InlineNotification
+          kind="success"
+          title="Valid UCAN."
+          subtitle="The UCAN is valid and has not expired."
+          lowContrast
+        />
+      </Column>
+    </Row>
   {/if}
 {/if}
 
 <Row>
   <Column>
-      {#if decodedUcan !== null}
-        <Row padding>
-          <Column>
-            <h3>Explanation</h3>
-          </Column>
-        </Row>
-        <Row>
-          <Column>
-            Please see the
-            <Link
-              href="https://datatracker.ietf.org/doc/html/rfc7519"
-              target="_blank"
-            >
-              JWT RFC
-            </Link>
-            and the
-            <Link
-              href="https://whitepaper.fission.codes/access-control/ucan/jwt-authentication"
-              target="_blank"
-            >
-              UCAN specification
-            </Link>
-            for more details.
-          </Column>
-        </Row>
-        <Row>
-          <Column>
-            <StructuredList>
-              <StructuredListHead>
-                <StructuredListRow head>
-                  <StructuredListCell head>Field</StructuredListCell>
-                  <StructuredListCell head>Long Name</StructuredListCell>
-                  <StructuredListCell head>Value</StructuredListCell>
-                  <StructuredListCell head>Details</StructuredListCell>
+    {#if decodedUcan !== null}
+      <Row padding>
+        <Column>
+          <h3>Explanation</h3>
+        </Column>
+      </Row>
+      <Row>
+        <Column>
+          Please see the
+          <Link
+            href="https://datatracker.ietf.org/doc/html/rfc7519"
+            target="_blank"
+          >
+            JWT RFC
+          </Link>
+          and the
+          <Link
+            href="https://whitepaper.fission.codes/access-control/ucan/jwt-authentication"
+            target="_blank"
+          >
+            UCAN specification
+          </Link>
+          for more details.
+        </Column>
+      </Row>
+      <Row>
+        <Column>
+          <StructuredList>
+            <StructuredListHead>
+              <StructuredListRow head>
+                <StructuredListCell head>Field</StructuredListCell>
+                <StructuredListCell head>Long Name</StructuredListCell>
+                <StructuredListCell head>Value</StructuredListCell>
+                <StructuredListCell head>Details</StructuredListCell>
+              </StructuredListRow>
+            </StructuredListHead>
+            <StructuredListBody>
+              {#each ucan.headerFields(decodedUcan) as row}
+                <StructuredListRow>
+                  <StructuredListCell noWrap>{row.field}</StructuredListCell>
+                  <StructuredListCell noWrap>{row.longName}</StructuredListCell>
+                  <StructuredListCell>{row.value}</StructuredListCell>
+                  <StructuredListCell>
+                    {row.details}
+                  </StructuredListCell>
                 </StructuredListRow>
-              </StructuredListHead>
-              <StructuredListBody>
-                {#each ucan.headerFields(decodedUcan) as row}
-                  <StructuredListRow>
-                    <StructuredListCell noWrap>{row.field}</StructuredListCell>
-                    <StructuredListCell noWrap
-                      >{row.longName}</StructuredListCell
-                    >
-                    <StructuredListCell>{row.value}</StructuredListCell>
-                    <StructuredListCell>
-                      {row.details}
-                    </StructuredListCell>
-                  </StructuredListRow>
-                {/each}
-                {#each ucan.payloadFields(decodedUcan) as row}
-                  <StructuredListRow>
-                    <StructuredListCell noWrap>{row.field}</StructuredListCell>
-                    <StructuredListCell noWrap
-                      >{row.longName}</StructuredListCell
-                    >
-                    <StructuredListCell>{row.value}</StructuredListCell>
-                    <StructuredListCell>
-                      {row.details}
-                    </StructuredListCell>
-                  </StructuredListRow>
-                {/each}
-              </StructuredListBody>
-            </StructuredList>
-          </Column>
-        </Row>
-      {/if}
+              {/each}
+              {#each ucan.payloadFields(decodedUcan) as row}
+                <StructuredListRow>
+                  <StructuredListCell noWrap>{row.field}</StructuredListCell>
+                  <StructuredListCell noWrap>{row.longName}</StructuredListCell>
+                  <StructuredListCell>{row.value}</StructuredListCell>
+                  <StructuredListCell>
+                    {row.details}
+                  </StructuredListCell>
+                </StructuredListRow>
+              {/each}
+            </StructuredListBody>
+          </StructuredList>
+        </Column>
+      </Row>
+    {/if}
   </Column>
 </Row>
 
