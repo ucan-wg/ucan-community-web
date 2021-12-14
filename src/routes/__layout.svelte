a6c3072fd3f6f2cbccac0d84433107819de92cc1
Jeff Griffiths <jeff@eko-recordings.ca>
2021-12-13 16:36:39 -0800

    styles scoped to markdown generated content

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,11 +6,10 @@
     Row, 
     Column
   } from 'carbon-components-svelte'
+
   import Header from '$components/Header.svelte'
   import Footer from '$components/Footer.svelte'
   import '$static/white.css'
-
-  // let theme: 'white' = 'white' as const
   let theme: 'white' = 'white' as const
 
 </script>
@@ -22,9 +21,7 @@
   <Grid>
     <Row>
       <Column>
-        <div class="markdown-document">
           <slot />
-        </div>
       </Column>
     </Row>
   </Grid>
@@ -33,26 +30,65 @@
 
 <style>
 
-:global(div.markdown-document > *) {
+:global(div.markdown-generated > p), 
+:global(div.markdown-generated > ul),
+:global(div.markdown-generated > pre) {
   /* border: 1px dashed blue; */
-  margin-bottom: 0.5em;
+  margin-bottom: 1.2em;
+  width: 95%;
 }
 
-:global(div.markdown-document) {
-  padding: 10px;  
+:global(div.markdown-generated > pre) {
+  background-color: #dedede;
+  padding: 5px;
+  overflow: auto;
 }
 
-:global(div.markdown-document), :global(div.markdown-document *) {
-  font-family:Arial, Helvetica, sans-serif
+:global(div.markdown-generated) {
+  padding: 10px;
 }
 
-:global(div.markdown-document h1), :global(div.markdown-document h2), :global(div.markdown-document h3) {
+:global(div.markdown-generated), 
+:global(div.markdown-generated p), 
+:global(div.markdown-generated ul li),
+:global(div.markdown-generated a) {
+  font-family:Arial, Helvetica, sans-serif;
+  font-size: large;
+  line-height: 1.4em;
+}
+
+:global(div.markdown-generated h1), :global(div.markdown-generated h2), :global(div.markdown-generated h3) {
   font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  margin-bottom: 0.8em;
 }
 
-:global(div.markdown-document ul li) {
+:global(div.markdown-generated ul li) {
   list-style: disc;
-  margin-left: 1em;
+  margin-left: 1.4em;
+}
+
+:global(div.markdown-generated a) {
+  color: blue;
+}
+
+:global(div.markdown-generated a:visited) {
+  color: rebeccapurple;
+}
+
+:global(div.markdown-generated a:hover) {
+  color: red;
+}
+
+:global(.\.eg-header) {
+  color: red;
+}
+
+:global(.eg-payload) {
+  color: green;
+}
+
+:global(.eg-sig) {
+  color: blue;
 }
 
 </style>
