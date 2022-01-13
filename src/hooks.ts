c9c801a4cf3888cba940aea164c0d52f71398857
Jeff Griffiths <jeff@eko-recordings.ca>
2022-01-13 13:39:14 -0800

    added hooks.ts file

---
+++ src/hooks.ts
@@ -0,0 +1,8 @@
+/** @type {import('@sveltejs/kit').Handle} */
+export async function handle({ request, resolve }) {
+	const response = await resolve(request, {
+		ssr: false
+	});
+
+	return response;
+}
